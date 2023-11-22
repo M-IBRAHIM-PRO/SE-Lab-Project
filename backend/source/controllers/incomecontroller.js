cca52facb2800524b338470608793491d7bbe119
Umair Saleem <126170701+us3366@users.noreply.github.com>
2023-11-22 18:06:54 +0500

    Complete CRUD Of Income

--- backend/source/controllers/incomecontroller.js
+++ backend/source/controllers/incomecontroller.js
@@ -15,10 +15,72 @@ const createincome = expressAsyncHandler(async(req ,res) =>
         });
         res.json(income);
     } catch (error) {
-        res.json(error);f
+        res.json(error);
+    }
+});
+
+
+
+//Fetch all income
+
+const fetchincome = expressAsyncHandler(async(req ,res) =>
+{
+    try {
+        const income = await income.find();
+        res.json(income);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+// fetch a single id
+const fetch_one_income = expressAsyncHandler(async(req ,res) =>
+{
+    const { id } = req?.params;
+    try {
+        const income = await income.findById(id);
+        res.json(income);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+
+//update income
+
+const update_income = expressAsyncHandler(async(req ,res) =>
+{
+    const { id } = req?.params;
+    const {title , amount , description} = req.body;
+    try {
+        const income = await income.findByIdAndUpdate(
+            id,
+            {
+                title,
+                description,
+                amount,
+            },
+               { new : true}
+        );
+        res.json(income);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+
+// delete a single id
+const delete_income = expressAsyncHandler(async(req ,res) =>
+{
+    const { id } = req?.params;
+    try {
+        const income = await income.findByIdAndDelete(id);
+        res.json(income);
+    } catch (error) {
+        res.json(error);
     }
 });
 
 module.exports = {
-    createincome
+    createincome,fetchincome,fetch_one_income,update_income,delete_income
 };
--- backend/source/routes/incomeroutes.js
+++ backend/source/routes/incomeroutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const incomeroutes = express.Router();
-const {createincome} = require('../controllers/incomecontroller');
+const {createincome,fetchincome, fetch_one_income,update_income,delete_income} = require('../controllers/incomecontroller');
 
 incomeroutes.post('/',createincome);
-
+incomeroutes.get('/',fetchincome);
+incomeroutes.get('./:id' , fetch_one_income);
+incomeroutes.put('./:id',update_income);
+incomeroutes.delete('./:id',delete_income);
 module.exports = incomeroutes;
