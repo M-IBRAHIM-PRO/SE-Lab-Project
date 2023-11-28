6572424bed8f465a5fe0b2aa9143d07f1a3cecd3
Umair Saleem <126170701+us3366@users.noreply.github.com>
2023-11-28 23:42:46 +0500

    Forgot Password

--- backend/source/server.js
+++ backend/source/server.js
@@ -1,8 +1,35 @@
-const http = require("http");
-const app = require("./app");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const PORT = process.env.PORT || 3010;
+const app = express();
 
-const server = http.createServer(app);
-
-server.listen(PORT, console.log(`Server is running on port ${PORT}` ));
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+// Routes Middleware
+app.use("/api/users", userRoute);
+// Error Middleware
+app.use(errorHandler);
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
